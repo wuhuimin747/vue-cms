5f3853f182957ad7da811fd9f7ace6c2ab3f5523
wuhuimin747 <747846472@qq.com>
2019-09-17 12:32:18 +0800

    商品的购买numberbox的联动

--- src/components/goods/GoodsInfo.vue
+++ src/components/goods/GoodsInfo.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>
+    <div class="goodsinfo-container">
+        <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
+            <div class="ball" v-show="ballFlag" ref="ball"></div>
+        </transition>
+
         <div class="mui-card">
             <div class="mui-card-content">
                 <div class="mui-card-content-inner">
@@ -13,21 +17,25 @@
             <div class="mui-card-content">
                 <div class="mui-card-content-inner">
                     <p class="price">
-                        <span>市场价：<del>￥{{goodsInfo.market_price}}</del>&nbsp;&nbsp;</span>
-                        <span>销售价：<span class="now_price">￥{{goodsInfo.sell_price}}</span></span>
+                        <span>市场价：
+                            <del>￥{{goodsInfo.market_price}}</del>&nbsp;&nbsp;</span>
+                        <span>销售价：
+                            <span class="now_price">￥{{goodsInfo.sell_price}}</span>
+                        </span>
                     </p>
                     <p class="price">
-                        <span>购买数量：</span><numberbox></numberbox>
+                        <span>购买数量：</span>
+                        <numberbox @getcount="getSelectedCount" :max="goodsInfo.stock_quantity"></numberbox>
                     </p>
                     <p>
                         <mt-button type="primary" size="small">立即购买</mt-button>
-                        <mt-button type="danger" size="small">加入购物车</mt-button>
+                        <mt-button type="danger" size="small" @click="addToShopCar">加入购物车</mt-button>
                     </p>
                 </div>
             </div>
         </div>
 
-         <div class="mui-card">
+        <div class="mui-card">
             <div class="mui-card-header">商品参数</div>
             <div class="mui-card-content">
                 <div class="mui-card-content-inner">
@@ -48,23 +56,20 @@
 import { Toast } from "mint-ui";
 import lunbotu from "../subcomponents/swiper.vue";
 import numberbox from "../subcomponents/numberbox.vue";
-import mui from "../../lib/mui/js/mui.min.js";
-
 
 export default {
     data() {
         return {
-            id : this.$route.params.id,
+            id: this.$route.params.id,
             lunboList: [],
-            goodsInfo : {},
+            goodsInfo: {},
+            ballFlag: false, // 控制小球的隐藏和显示的标识符
+            selectedCount: 1, //保存购买数量
         };
     },
     created: function() {
         this.getlunboList();
         this.getGoodsInfo();
-    },
-    mounted: function() {
-        mui(".mui-numbox").numbox();
     },
     methods: {
         getlunboList() {
@@ -82,7 +87,7 @@ export default {
                     }
                 });
         },
-        getGoodsInfo(){
+        getGoodsInfo() {
             this.$http
                 .get("api/goods/getinfo/" + this.id)
                 .then(function(result) {
@@ -93,38 +98,93 @@ export default {
                     }
                 });
         },
-        goDesc(id){ //商品介绍
-            this.$router.push({name : 'goodsDesc' , params : {id : this.id}})
+        goDesc(id) {
+            //商品介绍
+            this.$router.push({ name: "goodsDesc", params: { id: this.id } });
         },
-        goCmt(id){ //商品评论
-            this.$router.push({name : 'goodsCmt' , params : {id : this.id}})
-        }
+        goCmt(id) {
+            //商品评论
+            this.$router.push({ name: "goodsCmt", params: { id: this.id } });
+        },
+        addToShopCar() {
+            // 添加到购物车
+            this.ballFlag = !this.ballFlag;
+        },
+        beforeEnter(el) {
+            el.style.transform = "translate(0, 0)";
+        },
+        enter(el, done) {
+            el.offsetWidth;
 
+            // 小球动画优化思路：
+            // 1. 先分析导致 动画 不准确的 本质原因： 我们把 小球 最终 位移到的 位置，已经局限在了某一分辨率下的 滚动条未滚动的情况下；
+            // 2. 只要分辨率和 测试的时候不一样，或者 滚动条有一定的滚动距离之后， 问题就出现了；
+            // 3. 因此，我们经过分析，得到结论： 不能把 位置的 横纵坐标 直接写死了，而是应该 根据不同情况，动态计算这个坐标值；
+            // 4. 经过分析，得出解题思路： 先得到 徽标的 横纵 坐标，再得到 小球的 横纵坐标，然后 让 y 值 求差， x 值也求 差，得到 的结果，就是横纵坐标要位移的距离
+            // 5. 如何 获取 徽标和小球的 位置？？？   domObject.getBoundingClientRect()
+
+            // 获取小球的 在页面中的位置
+            console.info(this.$refs.ball);
+
+            const ballPosition = this.$refs.ball.getBoundingClientRect();
+            // 获取 徽标 在页面中的位置
+            const badgePosition = document
+                .getElementById("badge")
+                .getBoundingClientRect();
+
+            const xDist = badgePosition.left - ballPosition.left;
+            const yDist = badgePosition.top - ballPosition.top;
+
+            el.style.transform = `translate(${xDist}px, ${yDist}px)`;
+            el.style.transition = "all 0.5s cubic-bezier(.4,-0.3,1,.68)";
+            done();
+        },
+        afterEnter(el) {
+            this.ballFlag = !this.ballFlag;
+        },
+        getSelectedCount(count){
+            //获取numberbox子组件的购买数量
+            this.selectedCount = count;
+            console.info(this.selectedCount);
+            
+        }
     },
     components: {
         lunbotu: lunbotu,
-        numberbox : numberbox
+        numberbox: numberbox
     }
 };
 </script>
 
 <style lang="less" scoped>
-
-.price{
-    font-size: 14px;
-    color: black;
-    .now_price{
-        color: red;
-        font-size: 16px;
-        font-weight: bold;
+.goodsinfo-container {
+    .price {
+        font-size: 14px;
+        color: black;
+        .now_price {
+            color: red;
+            font-size: 16px;
+            font-weight: bold;
+        }
     }
-}
 
-.mui-card-footer{
-    display: block;
+    .mui-card-footer {
+        display: block;
 
-    button{
-        margin : 15px 0;
+        button {
+            margin: 15px 0;
+        }
+    }
+
+    .ball {
+        width: 15px;
+        height: 15px;
+        border-radius: 50%;
+        background-color: red;
+        position: absolute;
+        z-index: 99;
+        top: 390px;
+        left: 146px;
     }
 }
 </style>
--- src/components/subcomponents/numberbox.vue
+++ src/components/subcomponents/numberbox.vue
@@ -1,13 +1,31 @@
 <template>
-    <div class="mui-numbox" data-numbox-min='1'>
+    <div class="mui-numbox" data-numbox-min='1'  :data-numbox-max='max'>
         <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-        <input id="test" class="mui-input-numbox" type="number" value="1" />
+        <input id="test" class="mui-input-numbox" type="number" value="1" @change="getChangeedCount" ref="count"/>
         <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
     </div>
 </template>
 
 <script>
-export default {};
+import mui from "../../lib/mui/js/mui.min.js";
+
+export default {
+    props : ['max'],  //父组件向子组件传值，  数据源是异步操作  ， 会有响应时间的变化  undefined -> 确定值
+    mounted : function() {
+        mui(".mui-numbox").numbox();
+    },
+    methods : {
+        getChangeedCount(){
+            this.$emit('getcount' , this.$refs.count.value);  //子组件向父组件传值
+        }
+    },
+    watch : {
+        max : function(newval , oldval){ //max值的获取虽然是异步传输，但总有一个时刻会拿到，使用watch监听机制，将max赋给组件的最大值
+            //使用mui的js api 手动设置numberbox的最大值值
+            mui('.mui-numbox').numbox().setOption('max',newval)
+        }
+    }
+};
 </script>
 
 <style lang="less" scoped>
